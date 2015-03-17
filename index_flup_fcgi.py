72113690db35c04ea93459862a602a63d21a71fe
Gavin Kromhout <gavin@kromhouts.net>
2015-03-18 06:30:43 +1000

    Improve: clean up index.py cgi and fcgi options.

--- index_flup_fcgi.py
+++ index_flup_fcgi.py
@@ -3,6 +3,4 @@
 from flup.server.fcgi import WSGIServer
 from werkzeug_server import application
 
-if __name__ == '__main__':
-    # application = make_app()
-    WSGIServer(application).run()
+WSGIServer(application).run()
--- werkzeug_flup_cgi.py
+++
@@ -1,8 +0,0 @@
-#!env/bin/python
-
-from flup.server.cgi import WSGIServer
-from werkzeug_server import application
-
-if __name__ == '__main__':
-    # application = make_app()
-    WSGIServer(application).run()
